b3b8da0acf427e32e134ed527f3c06bf5060ef35
Aleksandr Sokolovskii <amsokol@gmail.com>
2017-06-22 22:35:08 +0300

    fixed background preloading

--- js/bulma.js
+++ js/bulma.js
@@ -1,10 +1,6 @@
 // The following code is based off a toggle menu by @Bradcomp
 // source: https://gist.github.com/Bradcomp/a9ef2ef322a8e8017443b626208999c1
 
-$(document).ready(function(){
-  $("#hero").css("backgroundImage", "url('../images/background.jpg')");
-});
-
 (function() {
     var burger = document.querySelector('.nav-toggle');
     var menu = document.querySelector('.nav-menu');
