33a04608ec9fa75a58d71669736120efaa8ad540
konoje91 <ambrela.dj@gmail.com>
2017-12-28 23:37:31 +0400

    test console log show-auto

--- js/bulma.js
+++ js/bulma.js
@@ -84,6 +84,7 @@ $(".car-modal-close").click(function() {
 });
 
 $(".show-auto-button").on('click', function () {
+    console.log($('.auto-list').length);
     $('.auto-list').show();
     $('.show-auto-button').hide();
 });
