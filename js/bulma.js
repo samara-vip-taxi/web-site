3661957bd2d7f416fdb5e22174d217d0a01ec93a
konoje91 <ambrela.dj@gmail.com>
2018-01-18 23:03:18 +0400

    add fix hidden block for bulma.js

--- js/bulma.js
+++ js/bulma.js
@@ -19,8 +19,8 @@ $(document).ready(function(){
 	});
 
     $(".show-auto-button").on('click', function () {
-        console.log($('.auto-list').length);
-        $('.auto-list').show();
+        console.log($(this));
+        $(this).siblings('.auto-list').show();
         $('.show-auto-button').hide();
     });
 });
